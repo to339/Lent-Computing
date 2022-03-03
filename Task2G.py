ffb8a2f7f8a333d1a59cfc0549ab0cb1e3700612
Thomas O'Hare <to339@cam.ac.uk>
2022-03-03 16:20:14 +0000

    2B deleted

--- Task2G.py
+++ Task2G.py
@@ -1,10 +1,19 @@
 from floodsystem.flood import flood_warnings, stations_level_over_threshold
 from floodsystem.stationdata import build_station_list
+from floodsystem.station import MonitoringStation
 
-yesterdays_date = 2/3/2022
-todays_date = 3/3/2022
+
 stations = build_station_list()
-r = stations_level_over_threshold(stations, tol)
 
-issue_flood_warnings = flood_warnings(yesterdays_date, todays_date, stations, r)
+
+station_name = "Cam"
+
+# Find station
+station_needed = None
+for station in stations:
+    if station.name == station_name:
+        station_needed = station
+        break
+
+issue_flood_warnings = flood_warnings(station_needed, stations, r)
 print (issue_flood_warnings)
