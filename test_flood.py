eaac1b396ee896c4cfd715f198e52a786fea0a6b
Thomas O'Hare <to339@cam.ac.uk>
2022-03-03 21:31:06 +0000

    Tests half complete

--- test_flood.py
+++ test_flood.py
@@ -3,9 +3,19 @@ from floodsystem.flood import *
 #Test 2B
 
 #Test 2C
+N = 10
+output2C = stations_highest_rel_level(list, N)
+check = len(output2C)
+if check == N:
+    print("2C Length Check: Pass")
+else:
+    print("2C Length Check: Fail")
 
 #Test 2E
+#I am unsure how to test a graph?
 
 #Test 2F
 
+
 #Test 2G
+#This has no inputs, unsure how to test this as well?
