5a10017d3f0029e3674e7bdfe229b346a09a22dd
Thomas O'Hare <to339@cam.ac.uk>
2022-03-03 14:03:36 +0000

    Need to know the output of 2B for 2C

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -1,6 +1,8 @@
 from haversine import haversine 
 from .utils import sorted_by_key
-import matplotlib
+import matplotlib.pyplot as plt
+from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.datafetcher import fetch_measure_levels
 
 #Task 2A - Completed for us
 
@@ -10,7 +12,7 @@ import matplotlib
 def stations_highest_rel_level(stations, N):
     #order the list of stations given by relative_water_level(self)
     #Print the first N stations from the list
-    ordered_stations = stations.sort()
+    ordered_stations = stations[N].sort()
     
     n=0
     at_risk=()
@@ -22,12 +24,28 @@ def stations_highest_rel_level(stations, N):
 #Task 2D - completed for us
 
 #Task 2E - Thomas to complete
-def plot_water_levels(station, dates, levels):
-    pass
+def plot_water_levels(stations, dates, levels):
+    
+    # Plot
+    plt.plot(dates, levels)
+
+    # Add axis labels, rotate date labels and add plot title
+    plt.xlabel('date')
+    plt.ylabel('water level (m)')
+    plt.xticks(rotation=45);
+    plt.title(stations.name)
+
+    # Display plot
+    plt.tight_layout()  # This makes sure plot does not cut off date labels
+
+    plt.show()
 
 #Task 2F: Chris to complete
 
 #Task 2G: Thomas to complete
-
-def random_sentence_to_remove_error():
-    pass
+    #If the ratio is over 1, issue a high flood warning
+    #If the ratio is over 1 and rising compared to yesterday's, issue a severe flood warning
+    #If the change in ratio is over 1, issue a severe flood warning
+    #If the change in ratio is over 0.75 issue a high flood warning
+    #If the ratio is over 0.5 and the change in ratio is over 0.5 issue a low flood warning
+    
