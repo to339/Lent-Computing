d7f0af0caa598d5a4117e98edef6daba560333db
Thomas O'Hare <to339@cam.ac.uk>
2022-03-07 10:25:42 +0000

    2E Finalised

--- test_flood.py
+++ test_flood.py
@@ -12,7 +12,27 @@ else:
     print("2C Length Check: Fail")
 
 #Test 2E
-#I am unsure how to test a graph?
+from datetime import datetime
+import matplotlib.pyplot as plt
+from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.datafetcher import fetch_measure_levels
+from floodsystem.flood import *
+import datetime
+
+# Build list of stations
+stations = build_station_list()
+N = 5
+level_values = stations_highest_rel_level(stations, N)
+# Update latest level data for all stations
+update_water_levels(stations)
+for stations in level_values:
+    dt = 2
+    dates, levels = fetch_measure_levels(stations.measure_id, dt=datetime.timedelta(days=dt))
+    plot_water_levels(stations, dates, levels)
+
+
+assert plot_water_levels(stations, dates, levels) == None
+print ("2E Output Check: Pass")
 
 #Test 2F
 
