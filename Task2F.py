62cdd4b2a826244325f3864f8ed242dd9d2a22cc
Thomas O'Hare <to339@cam.ac.uk>
2022-03-09 10:40:25 +0000

    Quick Fix

--- Task2F.py
+++ Task2F.py
@@ -8,19 +8,21 @@ import datetime
 
 from plot import plot_water_levels_with_fit
 
+def run():
+    stations = build_station_list()
 
-stations = build_station_list()
+    update_water_levels(stations)
+    x = stations_level_over_threshold(stations,0.8)
+    x.sort(key=lambda x:x[1])
+    top_5 = x[-6:]
+    print(top_5)
 
-update_water_levels(stations)
-x = stations_level_over_threshold(stations,0.8)
-x.sort(key=lambda x:x[1])
-top_5 = x[-6:]
-print(top_5)
-
-dt = 2
-for station, value in top_5:
-    dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
-    if dates:
-        print(polyfit(dates, levels, 4))
-        plot_water_levels_with_fit(station, dates, levels,4)
-    
+    dt = 2
+    for station, value in top_5:
+        dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
+        if dates:
+            print(polyfit(dates, levels, 4))
+            plot_water_levels_with_fit(station, dates, levels,4)
+        
+if __name__ == "__main__":
+    run()
