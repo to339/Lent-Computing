03925c63788d69da0450fae47395fb73a427bf12
Thomas O'Hare <to339@cam.ac.uk>
2022-03-09 10:33:03 +0000

    Neater Analysis

--- floodsystem/Analysis.py
+++ floodsystem/Analysis.py
@@ -2,27 +2,13 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib
-
-
-
-
-
-
-
     
 def polyfit(dates, levels, p):
     x = matplotlib.dates.date2num(dates)
-
     y = levels
-
-    
     d0 = x[0]
     p_coeff = np.polyfit(x-d0, y, p)
-    
-
 
     poly = np.poly1d(p_coeff)
-    
-
-    
+       
     return (poly,d0)
